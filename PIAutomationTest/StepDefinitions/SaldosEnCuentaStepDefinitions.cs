cc898c10bdd431f10ef3638d51ef0dad0da203ec
vidapogosoft <vidapogosoft@hotmail.com>
2023-08-31 12:13:13 -0500

    Update SaldosEnCuentaStepDefinitions.cs

--- PIAutomationTest/StepDefinitions/SaldosEnCuentaStepDefinitions.cs
+++ PIAutomationTest/StepDefinitions/SaldosEnCuentaStepDefinitions.cs
@@ -1,28 +1,48 @@
 using System;
 using TechTalk.SpecFlow;
 
+using BankAccountNS;
+using NUnit.Framework;
+
 namespace PIAutomationTest.StepDefinitions
 {
     [Binding]
     public class SaldosEnCuentaStepDefinitions
     {
-
+        public int beginningBalance;
+        public int debitAmount;
+        public double balance;
+        
         [Given(@"Mi saldo inicial de dolares en cuenta es de (.*)")]
-        public void GivenMiSaldoInicialDeDolaresEnCuentaEsDe(int p0)
+        public void GivenMiSaldoInicialDeDolaresEnCuentaEsDe(int montoinicial)
         {
-            throw new PendingStepException();
+            //Arrange
+            beginningBalance = montoinicial;
+
         }
 
         [When(@"Se debita un monto en dolares de (.*)")]
-        public void WhenSeDebitaUnMontoEnDolaresDe(int p0)
+        public void WhenSeDebitaUnMontoEnDolaresDe(int montodebito)
         {
-            throw new PendingStepException();
+            debitAmount = montodebito;
+
+            BankAccount account = new BankAccount("vidapogosoft", beginningBalance);
+
+            //Act
+
+            account.Debit(debitAmount);
+
+            //Asert
+
+            balance = account.Balance;
+
         }
 
         [When(@"Se debita un monto en dolares negativo de (.*)")]
         public void WhenSeDebitaUnMontoEnDolaresNegativoDe(int p0)
         {
-            throw new PendingStepException();
+
+            Assert.AreEqual("", "");
         }
 
         [When(@"Se acredita un monto en dolares de (.*)")]
@@ -32,9 +52,11 @@ namespace PIAutomationTest.StepDefinitions
         }
 
         [Then(@"Saldo actual en cuenta de (.*)")]
-        public void ThenSaldoActualEnCuentaDe(int p0)
+        public void ThenSaldoActualEnCuentaDe(double saldocuenta)
         {
-            throw new PendingStepException();
+            //Assert
+            Assert.AreEqual(balance, saldocuenta);
+
         }
 
     }
